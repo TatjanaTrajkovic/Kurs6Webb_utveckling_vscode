b5f92e48bb9468bec00d21def1b9195834ea8059
Tatjana <malina91983@gmail.com>
2026-08-26 17:16:29 +0200

    Javascript Dag2 array-object mappen uppdateras med ny kod

--- Javascript/Javascript Dag 2 (22:8)/javascript-dag2/array-object/js/javascript-exempel-array-object.js
+++ Javascript/Javascript Dag 2 (22:8)/javascript-dag2/array-object/js/javascript-exempel-array-object.js
@@ -1,6 +1,7 @@
 const elForm = document.querySelector('#formNewName');
 const elFNamn = document.querySelector('#txtFNamn');
 const elENamn = document.querySelector('#txtENamn');
+const elStad = document.querySelector('#txtStad');
 const elNamesOutput = document.querySelector('#namesOutput');
 const elFormOutput = document.querySelector('#formOutput');
 const elBtnShowNames = document.querySelector('#btnShowNames');
@@ -13,13 +14,14 @@ function newName(event){
     // Använd value när man hämtar från input i formuläret
     const fNamn = elFNamn.value;
     const eNamn = elENamn.value;
+    const stad = elStad.value;
 
     // lägger till namnet till arrayen
     names.push(
-        {forNamn: fNamn, efterNamn: eNamn}
+        {forNamn: fNamn, efterNamn: eNamn, stad: stad}
     );
 
-    elFormOutput.textContent = 'Du har lagt till: ' + fNamn + ' ' + eNamn;
+    elFormOutput.textContent = 'Du har lagt till: ' + fNamn + ' ' + eNamn + ' ' + stad;
 
     // Submittar inte formuläret
     event.preventDefault(); //
@@ -27,7 +29,7 @@ function newName(event){
 
 function arrayOfObject(){
     names.push(
-        {forNamn: 'Jerry', efterNamn: 'Garcia'}, {forNamn: 'Joni', efterNamn: 'Mitchell'}
+        {forNamn: 'Jerry', efterNamn: 'Garcia', stad: 'New York' }, {forNamn: 'Joni', efterNamn: 'Mitchell', stad: 'London'}
     );
 }
 
@@ -38,7 +40,7 @@ function showNames(){
    elFormOutput.textContent = ''; // Tömmer innehållet i p-taggen för meddelanden när man har lagt till ett nytt namn
 
     names.forEach(function(item, index, array) {
-        output = output + ' ' + item.forNamn + ' ' + item.efterNamn;
+        output = output + ' ' + item.forNamn + ' ' + item.efterNamn + ' ' + item.stad;
     })
     elNamesOutput.textContent = output;
 }
