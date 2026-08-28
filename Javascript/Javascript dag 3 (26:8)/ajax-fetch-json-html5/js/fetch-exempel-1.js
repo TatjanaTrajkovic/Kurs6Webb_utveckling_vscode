afd3a248a4fb39dbf6fa70814bc71f152b115fb2
Tatjana <malina91983@gmail.com>
2026-08-28 20:50:59 +0200

    Updaterar koden

--- Javascript/Javascript dag 3 (26:8)/ajax-fetch-json-html5/js/fetch-exempel-1.js
+++ Javascript/Javascript dag 3 (26:8)/ajax-fetch-json-html5/js/fetch-exempel-1.js
@@ -1,7 +1,12 @@
-fetch('data/data.json')
-    .then((response) => {
-        return response.json();
+fetch('data/data.json') //fetch() hämta filen
+    .then((response) => {   // när hämtningen är klar då gör nästa sak får vi tillabaka ett svar, det svaret kallas här för response
+        return response.json();// sen gör om svaret till javascript-data, dvs ta svaret och läs innehållet som json, omvandlar json-informationen så att js kan arbeta med den
+        //return skickar sedan resutltatet vidare till nästa .then
     })
-    .then((data) => {
-        console.log(data);
+    .then((data) => { //.then väntar på omvandlade svaret och när den är klar får vi resultatet här i data
+        console.log(data); //visa datan i console
     });
+
+
+
+
