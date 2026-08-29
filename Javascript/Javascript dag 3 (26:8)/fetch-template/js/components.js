fc0222289258a289e12b0f9c1e898d1c6c358306
Tatjana <malina91983@gmail.com>
2026-08-29 17:45:16 +0200

    JS dag3 fetch-template

--- Javascript/Javascript dag 3 (26:8)/fetch-template/js/components.js
+++ Javascript/Javascript dag 3 (26:8)/fetch-template/js/components.js
@@ -1,14 +1,18 @@
-async function loadComponent(selector, file) {
-    const element = document.querySelector(selector);
+async function loadComponent(selector, file) { //selector är element och fil som kommer visas
+    const element = document.querySelector(selector); //selector är id="about", id="header", id="footer" about, header, 
 
-    const response = await fetch(file);
+    const response = await fetch(file); //await väntar in våran omvandlad file info data
 
-    if (!response.ok) {
+    if (!response.ok) {         //om den går fel då har vi bara en felhanterings msg
         throw new Error(`Kunde inte ladda ${file}`);
     }
 
-    element.innerHTML = await response.text();
+    element.innerHTML = await response.text();//då kommer den att läsa igenom som vi har i våran fil
 }
 
-loadComponent("#header", "header.html");
+loadComponent("#header", "header.html");// man kallar på de funktionerna 2 gånger för de har olika id och olika innehåll
 loadComponent("#footer", "footer.html");
+loadComponent("#favorite", "favorite.html");
+
+
+//denna exempel visar att man kan fetcha filer som tex favorite.html och inte bara data.json
