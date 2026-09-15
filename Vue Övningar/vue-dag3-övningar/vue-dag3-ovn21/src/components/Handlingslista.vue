170e071fd1776d268fa8e3b6c7f885ba55b0ae38
Tatjana <malina91983@gmail.com>
2026-09-15 22:22:52 +0200

    Vue dag3 övning21

---
+++ Vue Övningar/vue-dag3-övningar/vue-dag3-ovn21/src/components/Handlingslista.vue
@@ -0,0 +1,33 @@
+
+<script setup>
+    import { ref } from 'vue'
+    
+    const newItem = ref('')
+    const items = ref([])
+
+    function addItem(){
+        items.value.push(newItem.value)
+        newItem.value = ''
+    }
+    function removeItem(index){
+        items.value.splice(index, 1)
+    }
+
+</script>
+
+
+<template>
+    <h1>Lista</h1>
+    <input type="text" v-model="newItem">
+     
+    <button @click="addItem">Add item</button>
+    
+
+    <ul>
+        <li v-for="(item, index) in items">
+            {{ item }} 
+            <button @click="removeItem(index)">Remove item</button>
+        </li>
+        
+    </ul>
+</template>
