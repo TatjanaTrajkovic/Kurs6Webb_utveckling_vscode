da796472471ddcf2e9acb8656b91da11f2aead2f
Tatjana <malina91983@gmail.com>
2026-09-15 10:31:27 +0200

    Vue-dag3-övning 15 och 16

---
+++ Vue Övningar/vue-dag3-övningar/vue-dag3-övn15/src/components/ClickCount.vue
@@ -0,0 +1,24 @@
+
+
+<script setup>
+
+    import { ref } from 'vue'
+
+    const antal = ref(0)
+
+    function count (){
+        antal.value++
+    }
+
+</script>
+
+
+<template>
+    <button @click.prevent="count">{{ antal }}</button>
+</template>
+
+
+<style scoped>
+
+
+</style>
