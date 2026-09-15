7bc0ab5cbc52085cb3cff0735deccf5b05a702aa
Tatjana <malina91983@gmail.com>
2026-09-15 14:03:05 +0200

    Vue dag3 övning 18

---
+++ Vue Övningar/vue-dag3-övningar/vue-dag3-övn18/src/components/Greeting.vue
@@ -0,0 +1,32 @@
+
+<script setup>
+
+    import { ref } from 'vue'
+
+    const name = ref('')
+    const greeting = ref('')
+
+    function showGreeting(){
+        greeting.value = `Hej ${name.value}!`
+    }
+
+</script>
+
+<template>
+
+
+    <p>{{ name }}</p>
+
+    <form @submit.prevent="showGreeting">
+        <input type="text" placeholder="Skriv ditt namn" v-model="name">
+        <button>Skicka</button>
+    </form>
+    
+    <p>{{ greeting }}</p>
+
+</template>
+
+
+
+
+
