bc965eac52d5f533e752be4806c2d4f2c8293da7
Tatjana <malina91983@gmail.com>
2026-09-15 10:54:10 +0200

    Vue dag3 övningn17

---
+++ Vue Övningar/vue-dag3-övningar/vue-dag3-övn17/src/components/ClickCount.vue
@@ -0,0 +1,28 @@
+
+<script setup>
+
+    import { ref } from 'vue'
+ 
+    const count = ref(0)
+
+    function showNewCount(){
+        if(count.value < 5){
+            count.value++
+        }else{
+
+        }
+    }
+
+</script>
+
+
+<template>
+
+    <button @click.prevent="showNewCount" :disabled="count >= 5">
+        Click: {{ count }}
+    </button>
+    <p>Antal klick: {{ count }}</p>
+</template>
+
+
+
