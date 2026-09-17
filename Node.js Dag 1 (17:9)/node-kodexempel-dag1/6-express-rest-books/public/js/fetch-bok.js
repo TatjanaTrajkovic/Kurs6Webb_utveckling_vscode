e83bf4e6a2c607d6413b4c2bb6a1bed4440e5fca
Tatjana <malina91983@gmail.com>
2026-09-17 20:06:51 +0200

    Node.js dag 1

---
+++ Node.js Dag 1 (17:9)/node-kodexempel-dag1/6-express-rest-books/public/js/fetch-bok.js
@@ -0,0 +1,33 @@
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+    return parent.appendChild(el);
+}
+
+const ul = document.querySelector('#bok');
+
+async function getBooks() {
+    //const url = 'data/data.json';
+  const url = "http://localhost:3000/books/";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+
+    let book = result;
+        return book.map(function(result) {
+            let li = createNode('li');
+            li.innerHTML = result.bokTitel + " " + result.bokForfattare;
+            append(ul, li);
+        })
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+ getBooks();
