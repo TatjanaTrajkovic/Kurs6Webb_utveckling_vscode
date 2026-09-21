b8fe724be1c9b9f4a07028d8b1a2b474ad11ac4f
Tatjana <malina91983@gmail.com>
2026-09-21 12:59:58 +0200

    Updates

--- Node.js Dag 2 (24sept)/node-kodexempel-dag2/3-node-mysql-version2/connectionMySQL.js
+++ Node.js Dag 2 (24sept)/node-kodexempel-dag2/3-node-mysql-version2/connectionMySQL.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2');
 const connectionMySQL = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'ditt lösenord',
-    database: 'din databas'
+    password: 'node1234',
+    database: 'demoNode20260917'
 });
 
 module.exports = connectionMySQL;
